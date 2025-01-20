b433bb92187417d9cb3b1352cb45cbac19ad695f
AstruntCM <mayer.marton.balint@gmail.com>
2025-01-20 21:19:18 +0100

    NUMPY

---
+++ 10 hét/10_het.py
@@ -0,0 +1,13 @@
+# NUMPY
+
+import numpy as np
+
+a = np.array([2, 3, 4]) # Numpy tömb
+
+a.ndim # Dimenzió check
+
+a.shape # Dimenzió mérete
+
+b = np.array([[3.0, 4, 5], [6, 7, 8]]) # 2 dimenziós tömb
+
+np.array([6, 7, 8], dtype='float32') # tipus beállitás
