72e60faead95f08f7159b1a812da454415c7ca18
AstruntCM <mayer.marton.balint@gmail.com>
2025-01-20 16:58:27 +0100

    Func-t, Premier League

--- 5 het/5_het.py
+++ 5 het/5_het.py
@@ -6,13 +6,13 @@ def root(x, n = 2):
 
 # Függvény a fv.-ben
 
-def f(y):
+def ftt(y):
     def g(x):
         return x * y
     return g
 
-g2 = f(2)
-g3 = f(3)
+g2 = ftt(2)
+g3 = ftt(3)
 
 #--------------------------------------------------------------------
 
@@ -55,30 +55,8 @@ sorted(freq, key = lambda x: -freq[x])
 
 # Premier League
 
-"""
 games = []
-
-ft = open('pre.txt')
-
-for i in range(6):
-    ft.readline()
-
-for sor in ft:
-    tok = sor.split('\t')
-    games.append({
-        'mérköz': int(tok[0]),
-        'hazai': tok[1],
-        'vendeg': tok[2],
-        'hazaigol': int(tok[3]),
-        'vendeggol': int(tok[4])
-    })
-
-ft.close()
-
-print(games)
-"""
-games = []
-f = open('pl.txt')
+f = open('5 het/pl.txt')
 for i in range(6): # első 6 sor átugrása
     f.readline()
 for line in f: # további sorok feldolgozása
@@ -91,3 +69,63 @@ for line in f: # további sorok feldolgozása
         'agoals': int(tok[4])
     })
 f.close()
+
+#------------------------------------------
+# 1. feladat
+# Mérközés hány % volt gól
+
+gol = 0
+
+for i in games:
+    if i['hgoals'] + i['agoals'] > 0:
+        gol += 1
+
+szazalek = gol / len(games) * 100
+
+#----------------------------------------
+
+# 2. feladat
+# Melyik mérközésen volt a legtöbb gól?
+
+gmax = -1
+
+for i in games:
+    gol = i['hgoals'] + i['agoals']
+    if gol > gmax:
+        gmax = gol
+        gbest = i
+
+print(gbest)
+
+#---------------------------------------
+
+n = 2
+
+stats = {}
+
+for g in games:
+    if g['round'] <= n:
+        hteam = g['hteam']
+        ateam = g['ateam']
+        hgoals = g['hgoals']
+        agoals = g['agoals']
+        
+        if hteam not in stats:
+            stats[hteam] = [0, 0, 0]
+        if ateam not in stats:
+            stats[ateam] = [0, 0, 0]
+        
+        if hgoals > agoals: # a hazai csapat nyert
+            stats[hteam][0] += 3
+        elif hgoals == agoals: # döntetlen
+            stats[hteam][0] += 1
+            stats[ateam][0] += 1
+        else: # a vendégcsapat nyert
+            stats[ateam][0] += 3
+            
+        stats[hteam][1] += hgoals - agoals
+        stats[ateam][1] += agoals - hgoals
+        stats[hteam][2] += hgoals
+        stats[ateam][2] += agoals
+
+print(stats)
